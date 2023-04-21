2765055f3e39655e2ab124ca2d83109ffc2d9253
AzizLajili <aziz.lajili@esprit.tn>
2023-04-21 16:08:10 +0100

    Both templates Integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllTemplateAdminComponent } from './backOffice/all-template-admin/all-template-admin.component';
+import { BodyAdminComponent } from './backOffice/body-admin/body-admin.component';
+import { AllTemplateUserComponent } from './frontOffice/all-template-user/all-template-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'admin',
+  component:AllTemplateAdminComponent,
+  children:[
+    {
+      path:'admin',
+      component:BodyAdminComponent
+    }
+  ]
+
+},
+{
+  path:'',
+  component:AllTemplateUserComponent,
+  /*children:[
+    {
+      path:'admin',
+      component:BodyAdminComponent
+    }
+  ]*/
+
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderAdminComponent } from './backOffice/header-admin/header-admin.component';
+import { FooterAdminComponent } from './backOffice/footer-admin/footer-admin.component';
+import { SideAdminComponent } from './backOffice/side-admin/side-admin.component';
+import { BodyAdminComponent } from './backOffice/body-admin/body-admin.component';
+import { AllTemplateAdminComponent } from './backOffice/all-template-admin/all-template-admin.component';
+import { FooterComponent } from './frontOffice/footer/footer.component';
+import { AllTemplateUserComponent } from './frontOffice/all-template-user/all-template-user.component';
+import { BodyComponent } from './frontOffice/body/body.component';
+import { HeaderComponent } from './frontOffice/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderAdminComponent,
+    FooterAdminComponent,
+    SideAdminComponent,
+    BodyAdminComponent,
+    AllTemplateAdminComponent,
+    FooterComponent,
+    AllTemplateUserComponent,
+    BodyComponent,
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
