08b15ea393ec1b01271d6684485dcb71de1b725a
ramikolsi <rami.kolsi@esprit.com>
2023-04-27 19:08:46 +0100

    last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AllTemplateAdminComponent } from './backOffice/all-template-admin/all-template-admin.component';
 import { BodyAdminComponent } from './backOffice/body-admin/body-admin.component';
 import { AllTemplateUserComponent } from './frontOffice/all-template-user/all-template-user.component';
+import {OrdonnanceComponent} from "./frontOffice/ordonnance/ordonnance.component";
+import {DelordonnanceComponent} from "./frontOffice/delordonnance/delordonnance.component";
+import {GetordonnanceComponent} from "./frontOffice/getordonnance/getordonnance.component";
+import {UploadimgOrdComponent} from "./frontOffice/uploadimg-ord/uploadimg-ord.component";
+import {UpdateOrdComponent} from "./frontOffice/update-ord/update-ord.component";
+import {GerercommandeComponent} from "./frontOffice/commande/gerercommande/gerercommande.component";
+import {UpdatecommandeComponent} from "./frontOffice/commande/updatecommande/updatecommande.component";
+import {AddcommandeComponent} from "./frontOffice/commande/addcommande/addcommande.component";
 
 const routes: Routes = [{
   path:'admin',
@@ -15,17 +23,38 @@ const routes: Routes = [{
   ]
 
 },
-{
-  path:'',
-  component:AllTemplateUserComponent,
-  /*children:[
-    {
-      path:'admin',
-      component:BodyAdminComponent
-    }
-  ]*/
+  { path:'ordonnance', component:OrdonnanceComponent},
+  {path:'delordonnance',component:DelordonnanceComponent},
+  {path:'getordonnances',component:GetordonnanceComponent},
+  {path:'uploadimgOrd',component:UploadimgOrdComponent},
+  {path:'updateordonnance',component:UpdateOrdComponent},
+  { path:'getcommands', component:GerercommandeComponent},
+  {path:'updatecommande',component:UpdatecommandeComponent},
+  // {path:'Commande', component:AddcommandeComponent},
 
-}];
+  // {path:'Commande', component:AddcommandeComponent,
+  // children:[{
+  //    path:'getcommands', component:GerercommandeComponent
+  // }]},
+
+
+  {
+    path: 'commande',
+    component: AllTemplateUserComponent,
+    children: [{
+      path: 'getcommands', component: GerercommandeComponent
+    }]
+  },
+  {
+    path: 'commande',
+    component: AllTemplateUserComponent,
+    children: [{
+      path: 'Commande', component: AddcommandeComponent
+    }]
+  }
+  ]
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderAdminComponent } from './backOffice/header-admin/header-admin.component';
@@ -12,6 +11,20 @@ import { FooterComponent } from './frontOffice/footer/footer.component';
 import { AllTemplateUserComponent } from './frontOffice/all-template-user/all-template-user.component';
 import { BodyComponent } from './frontOffice/body/body.component';
 import { HeaderComponent } from './frontOffice/header/header.component';
+import { OrdonnanceComponent } from './frontOffice/ordonnance/ordonnance.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {DatePipe} from "@angular/common";
+import { DelordonnanceComponent } from './frontOffice/delordonnance/delordonnance.component';
+import { GetordonnanceComponent } from './frontOffice/getordonnance/getordonnance.component';
+import { UploadimgOrdComponent } from './frontOffice/uploadimg-ord/uploadimg-ord.component';
+import { UpdateOrdComponent } from './frontOffice/update-ord/update-ord.component';
+import { DeletecommandeComponent } from './frontOffice/commande/deletecommande/deletecommande.component';
+import { UpdatecommandeComponent } from './frontOffice/commande/updatecommande/updatecommande.component';
+import { GerercommandeComponent } from './frontOffice/commande/gerercommande/gerercommande.component';
+import { AddcommandeComponent } from './frontOffice/commande/addcommande/addcommande.component';
+
+
 
 @NgModule({
   declarations: [
@@ -25,13 +38,24 @@ import { HeaderComponent } from './frontOffice/header/header.component';
     AllTemplateUserComponent,
     BodyComponent,
     HeaderComponent,
-    
+    OrdonnanceComponent,
+    DelordonnanceComponent,
+    GetordonnanceComponent,
+    UploadimgOrdComponent,
+    UpdateOrdComponent,
+    DeletecommandeComponent,
+    UpdatecommandeComponent,
+    GerercommandeComponent,
+    AddcommandeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
